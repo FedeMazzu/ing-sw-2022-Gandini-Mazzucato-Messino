3419ff9ce4b0c0be31d72924ffe488f6f20f32f5
zeno <nazzareno.messino@mail.polimi.it>
2022-04-07 14:07:15 +0200

    This commit adds cloud methods

--- src/main/java/it/polimi/deib/ingsw/gruppo44/Model/Cloud.java
+++ src/main/java/it/polimi/deib/ingsw/gruppo44/Model/Cloud.java
@@ -1,5 +1,6 @@
 package it.polimi.deib.ingsw.gruppo44.Model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -8,11 +9,47 @@ import java.util.Map;
  */
 
 public class Cloud {
+
     private Map<Color, Integer> students;
     private boolean empty;
     private int sizeMod;
 
-    public boolean isEmpty() {
-        return empty;
+    public Cloud(int playerNum){
+        int size;
+        if (playerNum == 3) size = 4;
+        else size = 3;
+        sizeMod = size;
+        students = new HashMap<>();
+        for (Color color : Color.values()) {
+            students.put(color, 0);
+        }
     }
-}
+
+
+        public boolean isEmpty() {return empty;}
+
+
+        public int getStudentsNum(Color color){
+            return students.get(color);
+        }
+
+        public void setStudents (Color color,int val){
+            students.put(color, val);
+        }
+
+        public int getSizeMod () {
+            return sizeMod;
+        }
+
+        /**
+         * method to wipe the clouds clean
+         */
+
+        public void wipeCloud(Cloud cloud){
+            for (Color color : Color.values()) {
+                students.remove(color, 4);
+            }
+            empty = true;
+        }
+
+    }
---
+++ src/test/java/it/polimi/deib/ingsw/gruppo44/Model/CloudTest.java
@@ -0,0 +1,4 @@
+package it.polimi.deib.ingsw.gruppo44.Model;
+
+public class CloudTest {
+}
